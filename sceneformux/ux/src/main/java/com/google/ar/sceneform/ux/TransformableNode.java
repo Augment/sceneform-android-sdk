753809bb22e9be24e8a8d88924451cbcf5c94444
Matthieu Negrier <matthieu.negrier@augment.com>
2020-12-17 21:24:31 +0100

    Update controllers

--- sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/TransformableNode.java
+++ sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/TransformableNode.java
@@ -23,9 +23,9 @@ import androidx.annotation.Nullable;
  * TransformationSystem}.
  */
 public class TransformableNode extends BaseTransformableNode {
-  private TranslationController translationController;
-  private ScaleController scaleController;
-  private RotationController rotationController;
+  private TransformationController<DragGesture> translationController;
+  private TransformationController<PinchGesture> scaleController;
+  private TransformationController<TwistGesture> rotationController;
 
   private final DetectedARPlanes detectedARPlanes;
 
@@ -49,43 +49,41 @@ public class TransformableNode extends BaseTransformableNode {
   }
 
   public TransformableNode(@NonNull TransformableNode other) {
-    this(other.getTransformationSystem(), other.detectedARPlanes);
-
-    this.transformableNodeListener = other.transformableNodeListener;
-
-    translationController.setListener(other.translationController.getListener());
-    translationController.setSurroundingsPlaneListener(other.translationController.getSurroundingsPlaneListener());
-    rotationController.setListener(other.rotationController.getListener());
-    scaleController.setListener(other.scaleController.getListener());
+    super(other.getTransformationSystem());
+    this.detectedARPlanes = other.detectedARPlanes;
+    this.translationController = other.translationController.copy();
+    this.rotationController = other.rotationController.copy();
+    this.scaleController = other.scaleController.copy();
+    setTransformableNodeListener(other.transformableNodeListener);
   }
 
   /** Returns the controller that translates this node using a drag gesture. */
-  public @NonNull TranslationController getTranslationController() {
+  public @NonNull TransformationController<DragGesture> getTranslationController() {
     return translationController;
   }
 
   /** Set the controller that translates this node using a drag gesture. */
-  public void setTranslationController(@NonNull TranslationController translationController) {
+  public void setTranslationController(@NonNull TransformationController<DragGesture> translationController) {
     this.translationController = translationController;
   }
 
   /** Returns the controller that scales this node using a pinch gesture. */
-  public @NonNull ScaleController getScaleController() {
+  public @NonNull TransformationController<PinchGesture> getScaleController() {
     return scaleController;
   }
 
   /** Set the controller that scales this node using a pinch gesture. */
-  public void setScaleController(@NonNull ScaleController scaleController) {
+  public void setScaleController(@NonNull TransformationController<PinchGesture> scaleController) {
     this.scaleController = scaleController;
   }
 
   /** Returns the controller that rotates this node using a twist gesture. */
-  public @NonNull RotationController getRotationController() {
+  public @NonNull TransformationController<TwistGesture> getRotationController() {
     return rotationController;
   }
 
   /** Set the controller that rotates this node using a twist gesture. */
-  public void setRotationController(@NonNull RotationController rotationController) {
+  public void setRotationController(@NonNull TransformationController<TwistGesture> rotationController) {
     this.rotationController = rotationController;
   }
 
@@ -96,16 +94,14 @@ public class TransformableNode extends BaseTransformableNode {
   public void setTransformableNodeListener(@Nullable TransformableNodeListener transformableNodeListener) {
     if (transformableNodeListener != null) {
       this.transformableNodeListener = transformableNodeListener;
-      translationController.setListener(transformableNodeListener.translationListener);
-      translationController.setSurroundingsPlaneListener(transformableNodeListener.surroundingsPlaneListener);
-      rotationController.setListener(transformableNodeListener.rotationListener);
-      scaleController.setListener(transformableNodeListener.scaleListener);
+      transformableNodeListener.attachListeners(translationController, InteractionListenerType.TRANSLATION);
+      transformableNodeListener.attachListeners(rotationController, InteractionListenerType.ROTATION);
+      transformableNodeListener.attachListeners(scaleController, InteractionListenerType.SCALE);
     } else {
       this.transformableNodeListener = null;
-      translationController.setListener(null);
-      translationController.setSurroundingsPlaneListener(null);
-      rotationController.setListener(null);
-      scaleController.setListener(null);
+      transformableNodeListener.detachListeners(translationController);
+      transformableNodeListener.detachListeners(rotationController);
+      transformableNodeListener.detachListeners(scaleController);
     }
   }
 
