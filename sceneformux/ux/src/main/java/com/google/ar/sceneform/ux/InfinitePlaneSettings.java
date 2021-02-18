a82ce0ac677386f29c7278294f3166af82f43df6
Matthieu Negrier <matthieu.negrier@augment.com>
2021-02-18 18:36:12 +0100

    Missing class

---
+++ sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/InfinitePlaneSettings.java
@@ -0,0 +1,6 @@
+package com.google.ar.sceneform.ux;
+
+public interface InfinitePlaneSettings {
+    /** Set the maximum allowed distance from point of view when computing intersection with infinite plane. */
+    void setInfinitePlaneIntersectionMaximumDistance(Float distance);
+}
