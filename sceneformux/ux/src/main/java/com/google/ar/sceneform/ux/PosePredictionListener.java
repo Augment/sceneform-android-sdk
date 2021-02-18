8bbd4c6cf666eb24efaa1ee8e11393c8cb7d57b5
Matthieu Negrier <matthieu.negrier@augment.com>
2021-02-18 18:25:25 +0100

    Modify signature

--- sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/PosePredictionListener.java
+++ sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/PosePredictionListener.java
@@ -4,7 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.ar.core.Pose;
+import com.google.ar.core.Trackable;
 
 public interface PosePredictionListener {
-    void onPosePreviewListener(@Nullable Pose pose, @NonNull Boolean isApplicable);
+    void onPosePreviewListener(@Nullable Pose pose, @Nullable Trackable trackable, @NonNull Boolean isApplicable);
 }
